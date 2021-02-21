35acb36b9278b67f23d83894b3a50de2002c5073
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 21:45:29 -0300

    Split Loop - now we apply it to another calculation being done inside the loop, totalAmount

--- src/index.js
+++ src/index.js
@@ -69,6 +69,9 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
+  }
+
+  for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
 
