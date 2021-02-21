0ed4ac4c269a4e0c876637f259286997d7f5df01
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 21:23:54 -0300

    Following Martin Fowler's programming pattern again. Giving more meaning to the parameters and changing the name of the variable that will be returned to 'result'

--- src/index.js
+++ src/index.js
@@ -34,14 +34,14 @@ function statement(invoice, plays) {
     return result;
   }
 
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
     // soma créditos por volume
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    result += Math.max(aPerformance.audience - 30, 0);
     // soma um crédito extra para cada dez espectadores de comédia
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 
   let totalAmount = 0;
