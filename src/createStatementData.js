f61ed2794ab31d1b2bab4617fa490bcd5c14863d
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-21 00:17:57 -0300

    Replace Constructor with Factory Function - because we want to apply polymorphism later, we need to introduce subclasses. To get the right subclass, we use a factory function to do that for us.

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -7,7 +7,7 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(
+    const calculator = createPerformanceCalculator(
       aPerformance,
       playFor(aPerformance)
     );
@@ -29,6 +29,10 @@ export default function createStatementData(invoice, plays) {
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0);
   }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 class PerformanceCalculator {
