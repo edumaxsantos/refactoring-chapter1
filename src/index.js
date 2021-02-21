9680afee3b6f830c82e0186062746d1d49cfc72c
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 21:46:12 -0300

    Slide Statements - moving down the declaration of totalAmount

--- src/index.js
+++ src/index.js
@@ -60,8 +60,6 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  let totalAmount = 0;
-
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -71,6 +69,7 @@ function statement(invoice, plays) {
     } seats)\n`;
   }
 
+  let totalAmount = 0;
   for (let perf of invoice.performances) {
     totalAmount += amountFor(perf);
   }
