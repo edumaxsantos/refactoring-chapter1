844ccc772ca33d0db399c03c51f864379addffa1
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 21:10:53 -0300

    FIXED: amountFor was outside statement() scope

--- src/index.js
+++ src/index.js
@@ -10,6 +10,29 @@ function statement(invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
+  function amountFor(aPerformance, play) {
+    let result = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
+  }
 
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -38,29 +61,5 @@ function statement(invoice, plays) {
 
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits`;
-  return result;
-}
-
-function amountFor(aPerformance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-
   return result;
 }
