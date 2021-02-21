34b747962bf1c41a898968028e344d9bb582f1df
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 23:29:04 -0300

    Change Function Declaration - actually we could have done this sooner, since we are not using plays anymore from the parameter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(statementData);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -48,7 +48,7 @@ function statement(invoice, plays) {
   }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
 
   for (let perf of data.performances) {
