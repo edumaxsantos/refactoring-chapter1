ce40498bc0137a01dbc0bae7cc0d4da5f5c093f8
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 23:26:20 -0300

    Move Function - now applied to amountFor()

--- src/index.js
+++ src/index.js
@@ -15,27 +15,13 @@ function statement(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
-}
-
-function renderPlainText(data, plays) {
-  let result = `Statement for ${data.customer}\n`;
-
-  for (let perf of data.performances) {
-    // exibe a linha para esta requisição
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    } seats)\n`;
-  }
-
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits`;
-  return result;
 
   function amountFor(aPerformance) {
     let result = 0;
@@ -60,6 +46,21 @@ function renderPlainText(data, plays) {
 
     return result;
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
+    // exibe a linha para esta requisição
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
+  }
+
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits`;
+  return result;
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -90,7 +91,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
