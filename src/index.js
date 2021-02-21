25b797bec14da36ea759b37d0e23c66507775f58
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-02-20 22:59:10 -0300

    Analyzing other arguments used by renderPlainText, then we start to replace invoice data and add it to the data parameter. Now we are using data.customer instead of invoice.customer

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ console.log(statement(invoices[0], plays));
 
 function statement(invoice, plays) {
   const statementData = {};
+  statementData.customer = invoice.customer;
   return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+  let result = `Statement for ${data.customer}\n`;
 
   for (let perf of invoice.performances) {
     // exibe a linha para esta requisição
